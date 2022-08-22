1e73032bdc4ede5874b729c71d3dae75087b5823
TimaVol <tima.voloshuk@gmail.com>
2022-08-22 11:21:56 +0300

    edit styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import User from "./pages/User";
+import Users from "./pages/Users";
 import "./styles/App.scss";
-
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Users />} />
+      <Route path="/:id" element={<User />} />
+    </Routes>
   );
 }
---
+++ src/pages/Users.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Users() {
+  return (
+    <>
+      <Link to={"/1"}>to somthing</Link>
+      <Link to={"/2"}>to somthing</Link>
+      <Link to={"/3"}>to somthing</Link>
+      <Link to={"/4"}>to somthing</Link>
+    </>
+  );
+}
